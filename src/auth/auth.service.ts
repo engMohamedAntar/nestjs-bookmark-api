f183fb66887b437d6db43bf16039986fac21afdc
MohamedAntar <aboantar852003@gmail.com>
2025-07-30 16:33:05 +0300

    returned a custom error msg for the signUp function using try catch

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,16 @@
 //auth.service.ts
 import * as bcrypt from 'bcrypt';
 
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { SignUpDto } from 'src/dto/signup.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserResponseDto } from 'src/dto/userResponse.dto';
+import { Prisma } from '@prisma/client';
+
 
 @Injectable()
 export class AuthService {
@@ -13,19 +19,27 @@ export class AuthService {
     return { msg: 'I have signed in' };
   }
 
-  async signUp(body: SignUpDto): Promise<UserResponseDto>{
+  async signUp(body: SignUpDto): Promise<UserResponseDto> {
     //get the password and hash it
-    const hash= await bcrypt.hash(body.password, 10);
-    //create the new user
-    const user= await this.prismaService.user.create({
+    const hash = await bcrypt.hash(body.password, 10);
+
+    try {
+      //create the new user
+      const user = await this.prismaService.user.create({
         data: {
-            email: body.email,
-            hash,
-            firstName: body.firstName,
-            lastName: body.lastName,
-        }
-    });
-    //return the created user
-    return new UserResponseDto(user);
+          email: body.email,
+          hash,
+          firstName: body.firstName,
+          lastName: body.lastName,
+        },
+      });
+      //return the created user
+      return new UserResponseDto(user);
+    } catch (err) {
+      
+      if (err instanceof Prisma.PrismaClientKnownRequestError&& err.code === 'P2002')
+        throw new BadRequestException('email already exist');
+      throw new InternalServerErrorException('Internal server error');
+    }
   }
 }
