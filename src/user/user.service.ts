9cbb5c3e4ec830f01ed98d5f5d1ba68bf050f8fa
MohamedAntar <aboantar852003@gmail.com>
2025-08-10 18:42:06 +0300

    added field filtering and keyword searching

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,26 +7,42 @@ export class UserService {
   constructor(private prismaService: PrismaService) {}
 
   async getAllUsers(query: any) {
-    const { sort, ...filter } = query;
+    const { sort, fields, keyword, ...filter } = query;
 
+    //keyword searching
+    if(keyword) {
+      filter.OR=[
+        {firstName: {contains: keyword, mode: 'insensitive'}},
+        {lastName: {contains: keyword, mode: 'insensitive'}},
+      ]
+    }
+    //sorting
     let orderBy: any = undefined;
-
     if (sort) {
       let field = sort;
-      let direction= 'asc';
+      let direction = 'asc';
 
-      // Allow descending if sort starts with "-"
-      if (sort.startsWith('-')) {
+      if (field.startsWith('-')) {
         field = sort.substring(1);
         direction = 'desc';
       }
-
       orderBy = { [field]: direction };
+    }
+
+    //field selection
+    let select: any = undefined;
+    if (fields) {
+      const fieldsArray = fields.split(',');
+      select = fieldsArray.reduce((acc, curr) => {
+        acc[curr] = true;
+        return acc;
+      }, {});
     }
 
     const users = await this.prismaService.user.findMany({
       where: filter,
       orderBy,
+      select, // {firstName: true, lastName: true}
     });
     return users;
   }
