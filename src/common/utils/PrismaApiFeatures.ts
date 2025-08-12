b8a5b52dcc1628fbf7e440058d106472e239e679
MohamedAntar <aboantar852003@gmail.com>
2025-08-13 00:49:35 +0300

    created PrismaApiFeatures class

---
+++ src/common/utils/PrismaApiFeatures.ts
@@ -0,0 +1,78 @@
+export class PrismaApiFeatures {
+  private filter: any;
+  private orderBy: any;
+  private select: any;
+  private take: number;
+  private skip: number;
+  private keyword: string;
+  private page: number;
+  constructor(
+    private query,
+    private searchFields: string[],
+  ) {
+    //extract the query
+    const { sort, fields, keyword, limit, page, ...filter } = query;
+    this.filter = filter;
+    this.keyword = keyword;
+    this.take = limit ? parseInt(limit) : 10;
+    this.skip = page ? (parseInt(page) - 1) * this.take : 0;
+    this.page = page ? parseInt(page) : 1;
+
+    this.setKeywordFilter();
+    this.setSorting(sort);
+    this.setFieldSelection(fields);
+  }
+
+  setKeywordFilter() {
+    if (this.keyword && this.searchFields.length > 0) {
+      this.filter.OR = this.searchFields.map((field) => ({
+        [field]: { contains: this.keyword, mode: 'insensitive' },
+      }));
+    }
+  }
+
+  setSorting(sort: string | undefined) {
+    if (sort) {
+      let field = sort;
+      let direction = 'asc';
+
+      if (field.startsWith('-')) {
+        field = sort.substring(1);
+        direction = 'desc';
+      }
+      this.orderBy = { [field]: direction };
+    }
+  }
+
+  setFieldSelection(fields: string | undefined) {
+    if (fields) {
+      const fieldsArray = fields.split(',');
+      this.select = fieldsArray.reduce((acc, curr) => {
+        acc[curr] = true;
+        return acc;
+      }, {});
+    }
+  }
+
+  buildOptions() {
+    return {
+      where: this.filter,
+      orderBy: this.orderBy,
+      select: this.select,
+      take: this.take,
+      skip: this.skip,
+    };
+  }
+
+  getPaginationInfo(total: number, resultsCount: number) {
+    const totalPages = Math.ceil(total / this.take);
+    return {
+      results: resultsCount, //number of results returned after the pagination
+      total, //all available docs
+      currentPage: this.page,
+      totalPages,
+      prev: this.page > 1 ? this.page - 1 : undefined,
+      next: this.page < totalPages ? this.page + 1 : undefined,
+    };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaApiFeatures } from 'src/common/utils/PrismaApiFeatures';
 
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -6,64 +7,14 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class UserService {
   constructor(private prismaService: PrismaService) {}
 
-  async getAllUsers(query: any) {
-    const { sort, fields, keyword, limit, page, ...filter } = query;
+  async getAllUsers(query: Record<string,any>) {
+    const apiFeature= new PrismaApiFeatures(query, ['firstName', 'lastName']);
+    const options= apiFeature.buildOptions();
 
-    //keyword searching
-    if (keyword) {
-      filter.OR = [
-        { firstName: { contains: keyword, mode: 'insensitive' } },
-        { lastName: { contains: keyword, mode: 'insensitive' } },
-      ];
-    }
-    //sorting
-    let orderBy: any = undefined;
-    if (sort) {
-      let field = sort;
-      let direction = 'asc';
+    const users= await this.prismaService.user.findMany(options);
+    const total= await this.prismaService.user.count({where: options.where});
 
-      if (field.startsWith('-')) {
-        field = sort.substring(1);
-        direction = 'desc';
-      }
-      orderBy = { [field]: direction };
-    }
-
-    //field selection
-    let select: any = undefined;
-    if (fields) {
-      const fieldsArray = fields.split(',');
-      select = fieldsArray.reduce((acc, curr) => {
-        acc[curr] = true;
-        return acc;
-      }, {});
-    }
-
-    //pagination
-    const take = limit ? parseInt(limit): 10;
-    const skip = page ? (parseInt(page)-1) * take : 0;
-
-    const users = await this.prismaService.user.findMany({
-      where: filter,
-      orderBy,
-      select, // {firstName: true, lastName: true}
-      take,
-      skip,
-    });
-
-    //paginationInfo
-    const total = await this.prismaService.user.count({ where: filter });
-    const totalPages = Math.ceil(total / take);
-    const currentPage= page? parseInt(page): 1;
-
-    const paginationInfo = {
-      results: users.length,
-      total,
-      currentPage,
-      totalPages,
-      prev: currentPage > 1 ? currentPage - 1 : undefined,
-      next: currentPage < totalPages ? currentPage + 1 : undefined,
-    };
+    const paginationInfo= apiFeature.getPaginationInfo(total ,users.length)
     return { paginationInfo, users };
   }
 
